9d64e5367bfb507fc4a9c00282606bdd6c740223
MikeSC27 <mcruzen89@gmail.com>
2022-11-14 14:19:21 -0800

    push secondary

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
-import Square from './components/Square'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import Square from './components/Square';
+
+
+// gameBoard set up, 3 by 3 grid 200px squares with a black boarder. 
+// 
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [gameBoard, setGameBoard] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ])
+  const [handleMark, setHandleMark] = useState()
 
   return (
     <>
-      <h1>Tic Tac Toe</h1>
-      <Square />
+    <h1>Tic Tac Toe</h1>
+    <div className="gameBoard">
+      {gameBoard.map((value, index) => {
+        return (
+          <Square 
+            value={value}
+            key={index}
+            index={index}
+            handleMark={handleMark}
+            />
+        )
+      })}
+    </div>
     </>
   )
 }
-
-export default App
