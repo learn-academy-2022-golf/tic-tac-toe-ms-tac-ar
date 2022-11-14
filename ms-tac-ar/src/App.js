e1b31fe747dfde3334914491f869bd1f8ea2a7a8
MikeSC27 <mcruzen89@gmail.com>
2022-11-14 14:15:34 -0800

    tic-tac-toe user story 1 grid complete

---
+++ ms-tac-ar/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+import Square from './components/Square';
+
+
+// gameBoard set up, 3 by 3 grid 200px squares with a black boarder. 
+// 
+
+const App = () => {
+  const [gameBoard, setGameBoard] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ])
+  const [handleMark, setHandleMark] = useState()
+
+  return (
+    <>
+    <h1>Tic Tac Toe</h1>
+    <div className="gameBoard">
+      {gameBoard.map((value, index) => {
+        return (
+          <Square 
+            value={value}
+            key={index}
+            index={index}
+            handleMark={handleMark}
+            />
+        )
+      })}
+    </div>
+    </>
+  )
+}
+
+
+
+
+
+export default App;
